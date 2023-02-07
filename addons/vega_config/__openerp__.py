41d30f16c89dc990c209d8cc1a7c591cf2d10f90
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2023-02-07 16:23:57 +0100

    [ADD] __openerp__.py for the default instance addon

---
+++ addons/vega_config/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'vega_config',
+    'summary': '''FS-Online vega instance configuration''',
+    'description': '''
+FS-Online Instance Configuration
+================================
+
+Customer configuration for the instance vega
+
+- Default settings
+- View modifications
+- CSS
+- Translations
+    ''',
+    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'fsonline',
+    ],
+    'data': [],
+}
+
+    
